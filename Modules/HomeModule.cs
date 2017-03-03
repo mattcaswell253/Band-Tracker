14ae4e2d1e6d1d38c37e151e3410d5aa2f524932
mattcaswell <matthewcaswell253@Gmail.com>
2017-03-03 13:11:26 -0800

    complete most of home module with routes

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -24,9 +24,63 @@ namespace BandTracker
               return View["index.cshtml", Venue.GetAll()];
             };
 
+            Post["/venue/{id}/band/new"] = parameters => {
+              Band newBand = new Band(Request.Form["band-name"]);
+              newBand.Save();
+              Venue SelectedVenue = Venue.Find(parameters.id);
+              SelectedVenue.AddBand(newBand);
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              List<Band> BandList = SelectedVenue.GetBands();
+              model.Add("venue", SelectedVenue);
+              model.Add("bands", BandList);
+
+              return View["bands_venues.cshtml", model];
+          };
+
+          Get["/venue/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              Venue SelectedVenue = Venue.Find(parameters.id);
+              List<Band> BandList = SelectedVenue.GetBands();
+              model.Add("venue", SelectedVenue);
+              model.Add("bands", BandList);
+              return View["bands_venues.cshtml", model];
+          };
+
+          Get["/band/{id}"] = parameters => {
+              Dictionary<string, object> model = new Dictionary<string, object>();
+              Band SelectedBand = Band.Find(parameters.id);
+              List<Venue> BandVenue = SelectedBand.GetVenues();
+              model.Add("band", SelectedBand);
+              return View["index.cshtml"];
+          };
+
+          Delete["/venue/delete/{id}"] = parameters => {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Venue SelectedVenue = Venue.Find(parameters.id);
+            SelectedVenue.Delete();
+            model.Add("venue", SelectedVenue);
+            return View["success.cshtml", model];
+          };
+
+          Get["/band/delete/{id}"] = parameters => {
+            Band SelectedBand = Band.Find(parameters.id);
+            return View["bands_venues.cshtml", SelectedBand];
+          };
+
+          Patch["/band/edit/{id}"] = parameters => {
+            Band SelectedBand = Band.Find(parameters.id);
+            SelectedBand.UpdateBand(Request.Form["band-name"]);
+            return View["success.cshtml", Band.GetAll()];
+          };
 
 
-
+          Delete["/band/delete/{id}"] = parameters => {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Band SelectedBand = Band.Find(parameters.id);
+            SelectedBand.Delete();
+            model.Add("band", SelectedBand);
+            return View["success.cshtml", model];
+          };
 
         }
 
--- Objects/Band.cs
+++ Objects/Band.cs
@@ -39,6 +39,39 @@ namespace BandTracker
     return _name;
   }
 
+  public void UpdateBand(string NewName)
+    {
+        SqlConnection conn = DB.Connection();
+        conn.Open();
+
+        SqlCommand cmd = new SqlCommand("UPDATE bands SET name = @NewName OUTPUT INSERTED.* WHERE id = @BandId;", conn);
+
+        cmd.Parameters.Add(new SqlParameter("@NewName", NewName));
+
+
+        SqlParameter bandIdParameter = new SqlParameter();
+        bandIdParameter.ParameterName = "@BandId";
+        bandIdParameter.Value = this.GetId();
+        cmd.Parameters.Add(bandIdParameter);
+
+        SqlDataReader rdr = cmd.ExecuteReader();
+
+        while(rdr.Read())
+        {
+            this._name = rdr.GetString(1);
+          
+        }
+
+        if(rdr != null)
+        {
+            rdr.Close();
+        }
+        if(conn != null)
+        {
+            conn.Close();
+        }
+    }
+
     public static List<Band> GetAll()
     {
       List<Band> allBands = new List<Band>{};
