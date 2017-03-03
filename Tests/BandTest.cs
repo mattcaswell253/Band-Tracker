453b7218a0e691d2c7fb39332881fc3e7b8ab3e6
mattcaswell <matthewcaswell253@Gmail.com>
2017-03-03 10:24:32 -0800

    add two more successfull tests to BandTest

--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -23,9 +23,36 @@ namespace BandTracker
           Assert.Equal(0, result);
         }
 
+        [Fact]
+      public void Test_Equal_ReturnsTrueIfDescriptionsAreTheSame()
+      {
+          //Arrange, Act
+          Band firstBand = new Band("Nirvana");
+          Band secondBand = new Band("Nirvana");
+
+
+          //Assert
+          Assert.Equal(firstBand, secondBand);
+      }
+
+      [Fact]
+      public void Test_Save_SavesToDatabase()
+      {
+          //Arrange
+          Band testBand = new Band("Nirvana");
+
+          //Act
+          testBand.Save();
+          List<Band> result = Band.GetAll();
+          List<Band> testList = new List<Band>{testBand};
+
+          //Assert
+          Assert.Equal(testList, result);
+        }
+
         public void Dispose()
       {
-          
+
           Band.DeleteAll();
       }
 
